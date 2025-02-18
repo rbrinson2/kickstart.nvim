059ac46a17b0d2deef98106fecefc7d7e7b7094e
rbrinson2 <48216680+rbrinson2@users.noreply.github.com>
2025-02-18 06:47:13 -0500

    Changed lualine theme

--- init.lua
+++ init.lua
@@ -977,6 +977,10 @@ require('lazy').setup({
   },
 })
 
-require('lualine').setup()
+require('lualine').setup {
+  options = {
+    theme = 'nord',
+  },
+}
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
