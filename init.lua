80c545ac64a42284f225ebe8db5e5d3929cdb0c0
rbrin <rbrinson2@gmail.com>
2025-03-17 08:49:59 -0400

    Trying to get an lsp to worth with verilog

--- init.lua
+++ init.lua
@@ -983,7 +983,9 @@ require('lualine').setup {
   },
 }
 
-require('lspconfig').verible.setup {}
+require('lspconfig').verible.setup {
+  cmd = { 'verible-verilog-ls', ' --rules_config_search', ' --indentation_spaces=4' },
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
