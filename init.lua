8c441af5dff4e3e523318c66fcb32d8371714ce3
rb <rbrinson2@gmail.com>
2025-05-26 17:10:29 -0400

    Added prefered tabs spacing

--- init.lua
+++ init.lua
@@ -1012,5 +1012,10 @@ require('lazy').setup({
   },
 })
 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
